d2bfda6dd1ff89bc0bf5153bc35ecf0fa4a11d89
nangxi <575981390@qq.com>
2020-04-10 18:41:43 +0800

    add filterToTextTransform

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 function filterToDateAndTime(data, type = 'DateAndTime', separator = '-') {
   let date = new Date(data);
-  let dateTime = `${date.getFullYear()}${separator}${date.getMonth() +
-    1}${separator}${date.getDate()}`;
+  let dateTime = `${date.getFullYear()}${separator}${
+    date.getMonth() + 1
+  }${separator}${date.getDate()}`;
   let hour = `${
     date.getHours() >= 10 ? date.getHours() : '0' + date.getHours()
   }`;
@@ -15,6 +16,10 @@ function filterToDateAndTime(data, type = 'DateAndTime', separator = '-') {
     return `${dateTime}`;
   } else if (type === 'Time') {
     return `${hour}:${minute}:${second}`;
+  } else if (type === 'Week') {
+    return ['周日', '周一', '周二', '周三', '周四', '周五', '周六'][
+      date.getDay()
+    ];
   } else {
     return `${dateTime} ${hour}:${minute}:${second}`;
   }
@@ -58,21 +63,14 @@ symbol 隔开符号
 
 function filterToSeparation(data, interval = 3, symbol = ',') {
   let returnVal = '';
-  let reverseVal = data
-    .toString()
-    .split('')
-    .reverse()
-    .join('');
+  let reverseVal = data.toString().split('').reverse().join('');
   let finVal;
   for (let i = 0; i < Math.ceil(reverseVal.length / interval); i++) {
     returnVal += `${reverseVal.slice(i * interval, (i + 1) * interval)}${
       i == Math.ceil(reverseVal.length / interval) - 1 ? '' : symbol
     }`;
   }
-  finVal = returnVal
-    .split('')
-    .reverse()
-    .join('');
+  finVal = returnVal.split('').reverse().join('');
   return finVal;
 }
 /*
@@ -85,10 +83,84 @@ function filterToHide(data, start, end, symbol = '*') {
   let val = data.toString().split('');
   return [...val.slice(0, start), ...item, ...val.slice(end)].join('');
 }
+
+/**
+ * 大小写转换
+ */
+
+function filterToTextTransform(data, type = 'AllInitialUpper') {
+  // let exp = new RegExp(/\s+[A-za-z]/,"g");
+  let exp = new RegExp(/\s+/, 'g');
+  let dataCopy = data.replace(exp, ' ');
+  let strArray = dataCopy.split(' ');
+  let symbol = [
+    ',',
+    '.',
+    '!',
+    '?',
+    ';',
+    '@',
+    '#',
+    '$',
+    '%',
+    '^',
+    '&',
+    '*',
+    '(',
+    ')',
+    '[',
+    ']',
+    '{',
+    '}',
+    '<',
+    '>'
+  ];
+  let symbolExp = new RegExp(
+    /(\,|\.}|\!|\?|\;|\@|\#|\$|\%|\^|\&|\*|\(|\)|\[|\]|\{|\}|\<|\>)(\s*|\n*)([a-zA-z])/,
+    'g'
+  );
+  let result;
+  if (type === 'AllInitialUpper' || type === 'AllInitialLower') {
+    result = strArray.reduce((total, item) => {
+      if (item[0] !== undefined) {
+        if (symbol.includes(item[0])) {
+          return `${total}${item[0]}${
+            type == 'AllInitialUpper'
+              ? item[1].toUpperCase()
+              : item[1].toLowerCase()
+          }${item.slice(2)} `;
+        } else {
+          return `${total}${
+            type == 'AllInitialUpper'
+              ? item[0].toUpperCase()
+              : item[0].toLowerCase()
+          }${item.slice(1)} `;
+        }
+      }
+      return total;
+    }, '');
+  } else if (type === 'InitialUpper' || type === 'InitialLower') {
+    dataCopy = dataCopy.replace(symbolExp, res => {
+      return `${res.slice(0, -1)}${
+        type == 'InitialUpper'
+          ? res.slice(-1).toUpperCase()
+          : res.slice(-1).toLowerCase()
+      }`;
+    });
+    result = `${
+      type == 'InitialUpper'
+        ? dataCopy.slice(0, 1).toUpperCase()
+        : dataCopy.slice(0, 1).toLowerCase()
+    }${dataCopy.slice(1)}`;
+  }
+  return result.trim();
+}
+
 export default {
   filterToDateAndTime,
   filterToDecimal,
   filterToSystem,
   filterToSeparation,
-  filterToHide
+  filterToHide,
+  filterToTextTransform
 };
